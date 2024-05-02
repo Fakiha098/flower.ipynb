01b4638996011244bf4c87caf2ab08e50d54df0e
Fakiha arshad <fakihaarshad23@gmail.com>
2024-05-02 02:59:23 -0700

    segments

---
+++ script.py
@@ -0,0 +1,15 @@
+print("hellow world")#this will print a string
+
+
+
+
+print("aammar")
+print("fakiha")
+print(3+4)#this will print a numbers +function
+
+varaible 
+x=3+4+5+6*3
+print(x)
+x+7
+x+3
+x+2
